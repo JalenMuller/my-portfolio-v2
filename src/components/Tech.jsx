8291a713e71c039f88b9737b453bf240e3781dea
JalenMuller <49196998+JalenMuller@users.noreply.github.com>
2023-06-16 05:07:35 +0200

    grouped balls
    grouped all balls and they now bounce

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,11 +3,13 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
+const icons = Object.values(technologies).map((technology) => technology.icon);
+
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      <div className="w-full">
-        <BallCanvas />
+      <div className="w-full h-[75vh] mx-auto">
+        <BallCanvas icons={icons} />
       </div>
     </div>
   );
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useMemo } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
@@ -7,33 +7,25 @@ import {
   Preload,
   useTexture,
 } from "@react-three/drei";
-import CanvasLoader from "../Loader";
-import { technologies } from "../../constants";
 
-const Ball = (props) => {
-  console.log(props);
-  const [decal] = useTexture([props.imgUrl]);
+import CanvasLoader from "../Loader";
+
+import * as THREE from "three";
+
+const Ball = ({ icon, position, rotation }) => {
+  const decal = useMemo(() => new THREE.TextureLoader().load(icon), [icon]);
+
   return (
-    <Float
-      speed={1.75}
-      position={[-2, 0, 0]}
-      rotationIntensity={1}
-      floatIntensity={2}
-    >
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color="#fff8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.03} />
+      <directionalLight position={[10, 5, -5]} />
+      <mesh castShadow receiveShadow scale={1} position={position}>
+        <icosahedronGeometry args={[1, 10]} />
+        <meshStandardMaterial color="#fff8eb" flatShading />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
+          scale={1.2}
           map={decal}
           flatShading
         />
@@ -42,17 +34,35 @@ const Ball = (props) => {
   );
 };
 
-const BallCanvas = ({ icon, i }) => {
+const BallCanvas = ({ icons }) => {
   return (
-    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+    <Canvas
+      frameloop="always"
+      shadows
+      dpr={[1, 1]}
+      camera={{ position: [0, 0, 5], fov: 75 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        {technologies.map((tech) => (
-          <Ball imgUrl={tech.icon} index={i} />
-        ))}
+        {/* <OrbitControls enableZoom={false} /> */}
+        <group>
+          {icons.map((icon, index) => {
+            const row = Math.floor(index / 3);
+            const col = index % 3;
+            return (
+              <Ball
+                key={icon}
+                icon={icon}
+                position={[col * 3 - 4, row * 3 + -2, -9]}
+              />
+            );
+          })}
+        </group>
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
 };
+
 export default BallCanvas;
